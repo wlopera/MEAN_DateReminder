6dc4adc06f0e9907ef99131e899e68f7a33412d6
William Lopera <lopera.william@gmail.com>
2019-06-16 11:28:27 -0500

    Agregar formato a la vista 1

--- angular/core.js
+++ angular/core.js
@@ -1,47 +1,65 @@
 
 var app = angular.module('mainApp', []);
 
-app.controller('mainController', function($scope, $http) {
+app.controller('mainController', function ($scope, $http) {
 
-    $scope.newReminder = {};
+	$scope.newReminder = {};
 	$scope.reminders = {};
 	$scope.selected = false;
 
 	// Obtener recordatorios de base de datos
 	$http.get('http://localhost:3000/api/reminder')
-		.success(function(data) {
+		.success(function (data) {
 			console.log("Consulta BD - core.js -- /api/reminder: ", data);
 			$scope.reminders = data;
 		})
-		.error(function(data, status, headers, config) {
+		.error(function (data, status, headers, config) {
 			console.log('##=> Error getReminders - data: %o', data);
 			console.log('##=> Error getReminders - status: %o', status);
 			console.log('##=> Error getReminders - headers: %o', headers);
 			console.log('##=> Error getReminders - config: %o', config);
-	});
+		});
 
 	// Registrar un recordatorio en BD
-	$scope.createReminder = function() {
+	$scope.createReminder = function () {
 		$http.post('http://localhost:3000/api/reminder', $scope.newReminder)
-		.success(function(data) {
-			$scope.newReminder = {};    // Borramos los datos iniciales del formulario
-			$scope.reminders = data;    // Pasamos los valores a la vista
-		})
-		.error(function(data) {
-			console.log('Error createReminder: ' + data);
-		});
+			.success(function (data) {
+				$scope.newReminder = {};    // Borramos los datos iniciales del formulario
+				$scope.reminders = data;    // Pasamos los valores a la vista
+			})
+			.error(function (data) {
+				console.log('Error createReminder: ' + data);
+			});
 
 	};
 
 	// Seleccionar recordatorio de la vista (tabla)
-	$scope.selectReminder = function(reminder) {
+	$scope.selectReminder = function (reminder) {
 		$scope.newReminder = angular.copy(reminder);
 		$scope.selected = true;
-		console.log("Recordatorio seleccionado: ", $scope.newReminder );
+		console.log("Recordatorio seleccionado: ", $scope.newReminder);
 	};
 
 	// Limpiar la entrada de datos
-	$scope.clear = function() {
+	$scope.clear = function () {
 		$scope.newReminder = {};
 	};
+
+	$('#addSymbol').on('click', function(){
+		
+		var myInput = $("#email");
+		myInput.val(myInput.val()+"@");
+		myInput.trigger('input');
+		myInput.focus();
+		
+	});
+
+	$('#addCom').on('click', function(){
+		var myInput = $("#email");
+		myInput.val(myInput.val()+".com");
+		myInput.trigger('input');
+		myInput.focus();
+	});
+
+
 });
